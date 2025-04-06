27f1836d1c966ac81563e817a334443b4cc3d340
Sourav Kumar <sauravsonukr@gmail.com>
2025-04-06 23:02:19 +0530

    Initial Commit

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -82,19 +82,19 @@ export const testimonialsData = [
 export const plans = [
     {
       id: 'Basic',
-      price: 90,
+      price: 10,
       credits: 100,
       desc: 'Best for personal use.'
     },
     {
       id: 'Advanced',
-      price: 250,
+      price: 50,
       credits: 500,
       desc: 'Best for business use.'
     },
     {
       id: 'Business',
-      price: 1000,
+      price: 250,
       credits: 5000,
       desc: 'Best for enterprise use.'
     },
